84d32d1a05b86c9462c3dcf72137afbcae850225
Pratham Kothari <kotharipratham52@gmail.com>
2025-10-21 18:42:12 +0530

    interview agent added

--- celery_worker.py
+++ celery_worker.py
@@ -9,5 +9,3 @@ load_dotenv()
 # The path is relative to the project root
 from backend.ML_models.ai_video_interview.utils.queue_utils import celery_app
 
-# This file is intentionally simple. Its job is just to make the Celery app instance
-# available for the Celery CLI to run.
